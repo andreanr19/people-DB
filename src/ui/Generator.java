7f1d0029c3b146dcaebb01243c6e019ede082627
kamneklogs <kamneklogs@gmail.com>
2020-10-27 16:25:09 -0500

    added some files

---
+++ src/ui/Generator.java
@@ -0,0 +1,21 @@
+package ui;
+
+public class Generator {
+
+
+    
+    public final static String PATH = "data/temporal.csv";
+    public int count;
+
+    public Generator() {
+    }
+    
+    public void generateData(int q){
+    //generar datos en txt (Ramdomizado basodo en los datasets)
+    }
+
+    public void sendToDB(){
+        //enviar File object a DBDriver para que este lea el archivo
+    }
+
+}
